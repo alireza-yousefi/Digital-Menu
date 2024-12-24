d4143f9ff48fbb368b2af8d7b67dfb713dc642d2
Alireza <alireza.ys245@gmail.com>
2024-12-24 17:04:34 +0330

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   ssr: false,
-  css: ["@/assets/css/font.css"],
+  css: ["@/public/css/font.css"],
   plugins: [{ src: "~/plugins/bootstrap", mode: 'client' }],
 
   modules: ["@nuxt/ui", '@pinia/nuxt'],
